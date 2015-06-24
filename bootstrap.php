de4cbfa2ab95a86164746ea14fbd153aaf84b1e8
Matthew Baggett <matthew@baggett.me>
2015-06-24 15:06:02 +0100

    Adding Travis config, removing from bootstrap

--- bootstrap.php
+++ bootstrap.php
@@ -6,14 +6,3 @@ define("APP_ROOT", rtrim(__DIR__,"/"));
 
 echo "getenv(\"HOST\") = " . getenv("HOST") . "\n";
 echo "\n";
-
-if(getenv("HOST") == "Travis"){
-  $database = new \Thru\ActiveRecord\DatabaseLayer(array(
-    'db_type'     => 'Mysql',
-    'db_hostname' => 'localhost',
-    'db_port'     => '3306',
-    'db_username' => 'travis',
-    'db_password' => 'travis',
-    'db_database' => 'tigerkit'
-  ));
-}
