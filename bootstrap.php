5b8c589e4c4d0fabb0fa8c6fc4ee87603e6ee2c6
Matthew Baggett <matthew@baggett.me>
2015-06-24 14:59:57 +0100

    Output host.

--- bootstrap.php
+++ bootstrap.php
@@ -4,6 +4,9 @@ require_once("vendor/autoload.php");
 
 define("APP_ROOT", rtrim(__DIR__,"/"));
 
+echo "getenv(\"HOST\") = " . getenv("HOST") . "\n";
+echo "\n";
+
 if(getenv("HOST") == "Travis"){
   $database = new \Thru\ActiveRecord\DatabaseLayer(array(
     'db_type'     => 'Mysql',
