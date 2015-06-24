00b38c14526b4261b8f6996f29edb9b31d4a6f1c
Matthew Baggett <matthew@baggett.me>
2015-06-24 14:57:15 +0100

    Fix travis mysql settings hopefully

--- bootstrap.php
+++ bootstrap.php
@@ -3,3 +3,14 @@
 require_once("vendor/autoload.php");
 
 define("APP_ROOT", rtrim(__DIR__,"/"));
+
+if(getenv("HOST") == "Travis"){
+  $database = new \Thru\ActiveRecord\DatabaseLayer(array(
+    'db_type'     => 'Mysql',
+    'db_hostname' => 'localhost',
+    'db_port'     => '3306',
+    'db_username' => 'travis',
+    'db_password' => 'travis',
+    'db_database' => 'tigerkit'
+  ));
+}
