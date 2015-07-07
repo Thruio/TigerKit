9362e809a44f9ffc37efe08b088f2f430f56cf65
Matthew Baggett <matthew@baggett.me>
2015-07-08 00:39:30 +0100

    Add initial boards support.

---
+++ src/Models/Post.php
@@ -0,0 +1,25 @@
+<?php
+namespace TigerKit\Models;
+
+use TigerKit\Services\ImageService;
+use Thru\ActiveRecord\ActiveRecord;
+
+/**
+ * Class Board
+ * @package TigerKit\Models
+ * @var $post_id INTEGER
+ * @var $board_id INTEGER
+ * @var $title VARCHAR(140)
+ * @var $url TEXT
+ * @var $body TEXT
+ */
+class Post extends UserRelatableObject
+{
+  protected $_table = "posts";
+
+  public $post_id;
+  public $board_id;
+  public $title;
+  public $url;
+  public $body;
+}
